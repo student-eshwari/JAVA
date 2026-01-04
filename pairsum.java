3f5a4a7c992da7ff158f7edc9353eb92c3062780
student eshwari <eshwarip18@gmail.com>
2026-01-04 22:08:36 +0530

    add pairsum

---
+++ pairsum.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class pairsum {
+    public  static boolean pairsum (int[] arr, int target)  {
+     HashSet<Integer>set = new HashSet<>();
+
+     for (int i=0;i<arr.length; i++) {
+        int complement=target-arr[i];
+
+        if (set.contains(complement)){
+            return true;
+        }
+        set.add(arr[i]);
+}
+return false;
+}
+public static void main(String[] args) {
+    int [] arr = {11,14,15,4,6,8,9,10,3};
+    int target = 16;
+    if(pairsum(arr, target)) {
+            System.out.println("true");
+    } else {
+            System.out.println("false");
+    }
+  }
+}
+
