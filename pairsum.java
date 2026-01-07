b9c4a5f265001058c0ac2f85da498827c367906d
student eshwari <eshwarip18@gmail.com>
2026-01-07 18:40:50 +0530

    add program

--- pairsum.java
+++ pairsum.java
@@ -1,25 +1,26 @@
 // 01-program
 import java.util.HashSet;
 public class pairsum {
-public  static boolean pairsum (int[] arr, int target)  {
-     HashSet<Integer>set = new HashSet<>();
+     public  static boolean pairsum (int[] arr, int target)  {
+           HashSet<Integer>set = new HashSet<>();
 
      for (int i=0;i<arr.length; i++) {
         int complement=target-arr[i];
 
-        if (set.contains(complement)){
-            return true;
+     if (set.contains(complement)){
+         return true;
         }
         set.add(arr[i]);
 }
-return false;
+        return false;
 }
+
 public static void main(String[] args) {
-    int [] arr = {11,14,15,4,6,8,9,10,3};
-    int target = 16;
-    if(pairsum(arr, target)) {
+       int [] arr = {11,14,15,4,6,8,9,10,3};
+       int target = 16;
+       if(pairsum(arr, target)) {
             System.out.println("true");
-    } else {
+    }  else {
             System.out.println("false");
     }
   }
--- profit.java
+++ profit.java
@@ -1,22 +1,22 @@
 // 02-program
 import java.util.*;
 public class profit {
-public static int maxprofit ( int[] price){
-   int buy = price[0];
-   int profit = 0;
+    public static int maxprofit ( int[] price){
+      int buy = price[0];
+      int profit = 0;
     
-   for (int i = 1; i < price.length; i++){
+      for (int i = 1; i < price.length; i++){
       if(price[i] < buy){
-          buy = price[i];
+      buy = price[i];
     
- } else if (price[i] - buy > profit){
-        profit = price[i] - buy;
+ }    else if (price[i] - buy > profit){
+      profit = price[i] - buy;
     }
     }
       return profit;
     }
-    public static void main (String[]args){
-    int [] price = { 4,7,8,9,3,2,1};
-    System.out.println(maxprofit(price));
+  public static void main (String[]args){
+      int [] price = { 4,7,8,9,3,2,1};
+      System.out.println(maxprofit(price));
     }
    }
