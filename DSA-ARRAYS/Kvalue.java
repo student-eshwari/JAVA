7aeb057ba6ef5e50be3a07d6b24256fc17b38c1f
student eshwari <eshwarip18@gmail.com>
2026-01-12 23:59:01 +0530

    add

--- DSA-ARRAYS/Kvalue.java
+++ DSA-ARRAYS/Kvalue.java
@@ -1,3 +1,15 @@
+import java.util.*;
 public class Kvalue {
+    public static int smallest(int[] arr, int k ) {
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,6,4,7,8,9,2,19,17};
+        int k = 4;
+        System.out.println(smallest(arr, k));
+    }
     
 }
+// time complexity : O(n log n)
+// space cmplexity : O(1)
