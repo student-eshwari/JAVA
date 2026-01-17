c5b61781213fe637e79aadea0cb599e5198d4c99
student eshwari <eshwarip18@gmail.com>
2026-01-17 23:14:55 +0530

    finding first non repeating character

---
+++ DSA-STRINGS/FirstNonRepeating.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class FirstNonRepeating {
+    public static char  EnglishLetters(String s) {
+        int [] freq = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)-'a']++;
+        
+        }
+        for(int i = 0; i < s.length(); i++) {
+            if(freq[s.charAt(i)-'a'] ==1) {
+                return s.charAt(i);
+            }
+        }
+        return '$';
+        }
+        public static void main(String[] args) {
+            String s = "madam";
+            System.out.println(EnglishLetters(s));
+        }
+    }
+    /* time complexity : O(n)
+       space complexity : O(1) */
+    
+
