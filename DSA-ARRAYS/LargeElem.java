07086354d08626f94ab95d0ccc339fb5036a2826
student eshwari <eshwarip18@gmail.com>
2026-05-01 16:28:09 +0530

    add

--- DSA-ARRAYS/LargeElem.java
+++ DSA-ARRAYS/LargeElem.java
@@ -1,18 +1,22 @@
-public class LargeElem{
-    public static int findmax(int[] nums) {
-        int max = nums[0];
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i] > max){
-                max = nums[i];
+public class LargeElem {
 
-        }
-    }
-    return max;
-}
     public static void main(String[] args) {
+
         int nums[] = {3,5,7,8,9,6};
+
         System.out.println("Largest Element is : " + findmax(nums));
     }
- }
-/** Time complexity : O(n)
-Space complexity : O(1) **/
+
+    public static int findmax(int nums[]) {
+
+        int max = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+}
