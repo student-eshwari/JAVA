7a7fda43eafd4e46cc768edf57df510cf8f8826f
student eshwari <eshwarip18@gmail.com>
2026-05-07 13:39:23 +0530

    add

--- DSA-STRINGS/reverse.java
+++ DSA-STRINGS/reverse.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class reverse {
-    public static String solution( String s) {
-        StringBuilder res = new StringBuilder();
-        for (int i = s.length()-1; i>=0; i--) {
+ public static String solution( String s) {
+ StringBuilder res = new StringBuilder();
+ for (int i = s.length()-1; i>=0; i--) {
             res.append(s.charAt(i));
-            
-        }
-        return res.toString();
+                    }
+    return res.toString();
     }
     public static void main(String[] args) {
         String s = "COMBINATION";
